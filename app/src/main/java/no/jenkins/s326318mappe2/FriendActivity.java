544da3be0549431564925b376effd8b4e75cfefb
Melfyn <melfyn1990@hotmail.com>
2019-10-21 15:59:34 +0200

    Add, update, delete friend

--- app/src/main/java/no/jenkins/s326318mappe2/FriendActivity.java
+++ app/src/main/java/no/jenkins/s326318mappe2/FriendActivity.java
@@ -2,7 +2,9 @@ package no.jenkins.s326318mappe2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 
 import no.jenkins.s326318mappe2.classes.Friend;
@@ -19,8 +21,8 @@ public class FriendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
-        inputName = (EditText) findViewById(R.id.r_name);
-        inputPhone = (EditText) findViewById(R.id.r_ph_no);
+        inputName = (EditText) findViewById(R.id.f_name);
+        inputPhone = (EditText) findViewById(R.id.f_ph_no);
         db = new DBHandler(this);
 
         if(getIntent().getExtras() != null){
@@ -38,10 +40,36 @@ public class FriendActivity extends AppCompatActivity {
     }
 
     public void loadBfriend(){
-
+        inputName.setText(bfriend.getName());
+        inputPhone.setText(bfriend.getPhoneNumber());
     }
 
     public void startKeyListeners(){
+        findViewById(R.id.add_friend).setOnClickListener(view -> addFriend());
+        findViewById(R.id.update_friend).setOnClickListener(view -> updateFriend());
+        findViewById(R.id.delete_friend).setOnClickListener(view -> deleteFriend());
+    }
 
+    public void addFriend(){
+        Friend friend = new Friend(inputName.getText().toString(), inputPhone.getText().toString());
+        db.addFriend(friend);
+        Log.d("Legg inn: ", "legger til kontakter");
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+
+    public void updateFriend(){
+        Friend friend = new Friend(bfriend.get_ID(), inputName.getText().toString(), inputPhone.getText().toString());
+        db.updateFriend(friend);
+        Log.d("Update: ", "Update friend");
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+
+    public void deleteFriend(){
+        db.deleteFriend(bfriend.get_ID());
+        Log.d("Delete", "Delete friend");
+        setResult(Activity.RESULT_OK);
+        finish();
     }
 }
--- app/src/main/java/no/jenkins/s326318mappe2/RestaurantActivity.java
+++ app/src/main/java/no/jenkins/s326318mappe2/RestaurantActivity.java
@@ -41,6 +41,11 @@ public class RestaurantActivity extends AppCompatActivity {
             loadBres();
             View b = findViewById(R.id.add_restaurant);
             b.setVisibility(View.GONE);
+        } else {
+            View b = findViewById(R.id.delete_restaurant);
+            b.setVisibility(View.GONE);
+            View bTwo = findViewById(R.id.update_restaurant);
+            bTwo.setVisibility(View.GONE);
         }
 
         startKeyListeners();
@@ -57,15 +62,31 @@ public class RestaurantActivity extends AppCompatActivity {
 
     public void startKeyListeners() {
         findViewById(R.id.add_restaurant).setOnClickListener(view -> addOneRestaurant());
+        findViewById(R.id.update_restaurant).setOnClickListener(view -> updateOneRestaurant());
+        findViewById(R.id.delete_restaurant).setOnClickListener(view -> deleteRestaurant());
     }
 
     public void addOneRestaurant() {
-        setResult(Activity.RESULT_OK);
         Restaurant res = new Restaurant(inputName.getText().toString(), inputAdress.getText().toString(), inputPhone.getText().toString(),inputType.getText().toString());
         db.addRestaurant(res);
         Log.d("Legg inn: ", "legger til kontakter");
+        setResult(Activity.RESULT_OK);
         finish();
     }
 
+    public void updateOneRestaurant() {
+        Restaurant res = new Restaurant(bres.get_ID(), inputName.getText().toString(), inputAdress.getText().toString(), inputPhone.getText().toString(),inputType.getText().toString());
+        db.updateRestaurant(res);
+        Log.d("Update: ", "Update restaurant");
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+
+    public void deleteRestaurant(){
+        db.deleteRestaurant(bres.get_ID());
+        Log.d("Delete", "Delete restaurant");
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
 
 }
